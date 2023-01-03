36a64afea392f78e0708242c5bbe8ee95e75a3b8
Dashkevich-Ann <dashkevich.ann.iv@gmail.com>
2023-01-03 14:27:51 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let numToString = n.toString();
+  if (numToString[0] != '-') {
+    let reversed = numToString.split('').reverse();
+    return +reversed.join('');
+  } else {
+    let arr = numToString.split('');
+    let minus = arr.shift();
+    let reversed = arr.reverse();
+    return +reversed.join('');
+  };
 }
